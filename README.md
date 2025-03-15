022b44ac00657b1c5b7a73f637897d4aead8b5e7
Mkhabele Mikejunior Mark Mashaole <mimkhabjhb024@student.wethinkcode.co.za>
2025-03-15 06:51:11 +0200

    unit testing

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # HelloJUnitTest
+
+
+HelloJUnit is a simple Java project demonstrating the use of **Maven** and **JUnit 5** for unit testing in **IntelliJ IDEA**.
+
+## 📌 Features
+- Maven-based project structure
+- Basic JUnit 5 test cases
+- Simple Java class with testable methods
+
+ 
---
+++ src/test/java/learning/junit/HelloJUnitTests.java
@@ -0,0 +1,22 @@
+package learning.junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HelloJUnitTests {
+
+    @Test
+    public void passingTest() {
+        HelloJUnit helloJUnit = new HelloJUnit();
+        assertEquals(6, helloJUnit.sum(4,2));
+    }
+
+    @Test
+    public void failingTest() {
+        HelloJUnit helloJUnit = new HelloJUnit();
+        assertEquals("Welcome, World",
+                helloJUnit.greeting("World"));
+    }
+}
